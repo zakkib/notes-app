4b94809e48436cce08d9495c0d63ae508793f9d8
zakkib <zzakkibaldani@gmail.com>
2025-02-09 21:23:25 +0700

    feat/create-notes
    added create notes feature that added the notes to the database and also to the client dashboard immediately

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,7 +1,7 @@
 // import PocketBase from 'pocketbase';
 import Link from 'next/link';
 import styles from './Notes.module.css';
-import CreateNote from './Create';
+import CreateNote from './CreateNote';
 
 async function getNotes() {
     const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
@@ -14,31 +14,32 @@ async function getNotes() {
 }
 
 export default async function NotesPage() {
-
     const notes = await getNotes();
-
+  
     return(
-        <div>
-            <h1>Notes</h1>
-            <div>
-                {notes?.map((note) => {
-                    return <Note key={note.id} note={note} />;
-                })}
-            </div>
+      <div>
+        <h1>Notes</h1>
+        <div className={styles.grid}>
+          {notes?.map((note) => {
+            return <Note key={note.id} note={note} />;
+          })}
         </div>
+  
+        <CreateNote />
+      </div>
     );
-}
+  }
 
-function Note({ note}: any) {
+function Note({ note }: any) {
     const { id, title, content, created } = note || {};
-
+  
     return (
       <Link href={`/notes/${id}`}>
-          <div>
-              <h2>{title}</h2>
-              <h5>{content}</h5>
-              <p>{created}</p>
-          </div>
+        <div className={styles.note}>
+          <h2>{title}</h2>
+          <h5>{content}</h5>
+          <p>{created}</p>
+        </div>
       </Link>
     );
-}
+  }
